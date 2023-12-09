4e9b0e5d93fb6f4ee8ba257073710d43d06bd8db
Shuchith <01fe21bci029@kletech.ac.in>
2023-12-09 15:30:19 +0530

    initial

---
+++ trackBUddy-api/routes/exercises.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const Exercises = require('../models/exercises.model');
+
+
+router.route('/').get((req,res)=>{
+    Exercises.find()
+        .then(exercises => res.json(exercises))
+        .catch(err => res.status(400).json('Erro:'+ err));
+});
+
+router.route('/add').post((req,res)=>{
+    const username = req.body.username;
+    const description = req.body.description;
+    const duration = Number(req.body.duration);
+    const date = Date.parse(req.body.date);
+
+    const newExercises = new Exercises({
+        username,
+        description,
+        duration,
+        date
+    });
+
+    newExercises.save()
+        .then(()=>res.json('Exercise added!'))
+        .catch(err => res.status(400).json('Error:'+err))
+});
+
+router.route('/:id').get((req,res)=>{
+    Exercises.findById(req.params.id)
+        .then(exercise => exercise)
+        .catch(err => res.status(400).json('Error:'+err))
+})
+
+module.exports = router;
