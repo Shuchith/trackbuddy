0ba1bce1984a64f058ac6fae3359b02b98376a42
Shuchith <01fe21bci029@kletech.ac.in>
2023-12-10 10:19:28 +0530

    new

--- trackBUddy-api/routes/exercises.js
+++ trackBUddy-api/routes/exercises.js
@@ -28,8 +28,31 @@ router.route('/add').post((req,res)=>{
 
 router.route('/:id').get((req,res)=>{
     Exercises.findById(req.params.id)
-        .then(exercise => res.json(exercise))
+        .then(exercise => exercise)
         .catch(err => res.status(400).json('Error:'+err))
 })
 
+router.route('/:id').delete((req,res)=>{
+    Exercises.findByIdAndDelete(req.params.id)
+        .then(()=>res.json("Exercise deleted" ))
+        .catch(err=>res.status(400).json('Error:' + err));
+})
+
+//update exercises by Id
+router.route('/update/:id').post((req,res)=>{
+    Exercises.findById(req.params.id)
+        .then(exercise=>{
+            console.log("req",req.body);
+            console.log(exercise,"exe");
+
+            exercise.username = req.body.username;
+            exercise.description = req.body.description;
+            exercise.duration = req.body.duration;
+            exercise.date = Date.parse(req.body.date)
+            exercise.save()
+                .then(()=> res.json('Exercise Updated!'))
+                .catch(err => res.status(400).json('Error: '+err));
+        })
+        .catch(err => res.status(400).json('Error:'+err))
+})
 module.exports = router;
